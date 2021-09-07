d5adf550b4d8ca45236ae0dc7d14be5c925c7b00
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2021-09-07 15:37:47 +0200

    dev: added static files to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 const path = require("path");
 
 module.exports = {
@@ -16,6 +17,9 @@ module.exports = {
     }),
     new MiniCssExtractPlugin({
       filename: "bundle.css",
+    }),
+    new CopyWebpackPlugin({
+      patterns: [{ from: "src/assets", to: "assets" }],
     }),
   ],
   module: {
