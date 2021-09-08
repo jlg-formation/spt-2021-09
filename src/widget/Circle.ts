537e1a349e02839c1bbda4991ea62a98c8f81d04
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2021-09-08 15:01:22 +0200

    dev: style circle

--- src/widget/Circle.ts
+++ src/widget/Circle.ts
@@ -1,7 +1,19 @@
+import { DrawingBoard } from "../DrawingBoard";
 import { Widget } from "../Widget";
 
 export class Circle extends Widget {
-  constructor() {
-    super();
+  depose(board: DrawingBoard) {
+    const r = 30;
+    const { x: cx, y: cy } = board.curPos;
+    const circle = document.createElementNS(
+      "http://www.w3.org/2000/svg",
+      "circle"
+    );
+
+    circle.setAttribute("cx", "" + cx);
+    circle.setAttribute("cy", "" + cy);
+    circle.setAttribute("r", "" + r);
+
+    board.contentElt.appendChild(circle);
   }
 }
