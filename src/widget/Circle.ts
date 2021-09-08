3b272607dc5e35d59af5f4490223ab8e4637cc52
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2021-09-08 15:28:35 +0200

    dev: added circle selection

--- src/widget/Circle.ts
+++ src/widget/Circle.ts
@@ -3,6 +3,11 @@ import { Widget } from "../Widget";
 
 export class Circle extends Widget {
   depose(board: DrawingBoard) {
+    this.deposeContent(board);
+    this.deposeSelection(board);
+  }
+
+  deposeContent(board: DrawingBoard) {
     const r = 30;
     const { x: cx, y: cy } = board.curPos;
     const circle = document.createElementNS(
@@ -15,5 +20,19 @@ export class Circle extends Widget {
     circle.setAttribute("r", "" + r);
 
     board.contentElt.appendChild(circle);
+  }
+  deposeSelection(board: DrawingBoard) {
+    const r = 30;
+    const { x: cx, y: cy } = board.curPos;
+    const circle = document.createElementNS(
+      "http://www.w3.org/2000/svg",
+      "circle"
+    );
+
+    circle.setAttribute("cx", "" + cx);
+    circle.setAttribute("cy", "" + cy);
+    circle.setAttribute("r", "" + r);
+
+    board.selectionElt.appendChild(circle);
   }
 }
